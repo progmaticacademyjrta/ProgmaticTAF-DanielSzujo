31e421d1b6a73661c0140ff85a73007c7589af19
szujodaniel <szujodaniel01@gmail.com>
2023-02-04 16:21:06 +0100

    Update

--- src/test/java/hu/progmatic/pages/HomePage.java
+++ src/test/java/hu/progmatic/pages/HomePage.java
@@ -10,6 +10,7 @@ public class HomePage {
     WebDriver driver;
     WebDriverWait wait;
     By headerBy = By.id("top");
+    By makeAppointmentBy = By.id("btn-make-appointment");
 
     public HomePage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
@@ -20,6 +21,13 @@ public class HomePage {
         driver.get("https://katalon-demo-cura.herokuapp.com");
         WebElement headerElement = driver.findElement(headerBy);
         Assert.assertTrue(headerElement.isDisplayed());
-        System.out.println("Homepage loaded properly.");
+        System.out.println("Homepage loaded properly");
+    }
+
+    public void makeAppointmentClick() {
+        WebElement makeAppointmentButton = driver.findElement(makeAppointmentBy);
+        makeAppointmentButton.click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://katalon-demo-cura.herokuapp.com/profile.php#login");
+        System.out.println("Make Appointment button is working");
     }
 }
